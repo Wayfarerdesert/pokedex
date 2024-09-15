474ae35d82b202ac39eacc8a719eeabd639f25f5
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-09-15 09:33:43 +0200

    Initial project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 w-full max-w-5x1 items-center justify-between text-sm lg:flex">
+              <h2 className="text-3xl text-bold">Pokémon Finder</h2>
+            </div>
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
